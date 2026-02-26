44f007fc4788e9f9b774a130234e8e0e641022f7
VMan-2002 <vman.2002.au@gmail.com>
2026-02-26 22:44:18 +1100

    soos 2 (aa)

--- lua/drawstep_radioglow.lua
+++ lua/drawstep_radioglow.lua
@@ -40,7 +40,7 @@ SMODS.DrawStep {
 				print("Radioglow sprite "..(rd.spr).." not found")
 				card.ecattos_radioglow[4] = "default"
 			end
-			if not topuplib.viewedFromCollection(card) then
+			if card.area and not topuplib.viewedFromCollection(card) then
 				for k,v in pairs(card.area.cards) do
 					if v.config.center_key == "j_ecattos_purrcent" then
 						elementcattos.purrcentCopyRadioactive(card, v)
