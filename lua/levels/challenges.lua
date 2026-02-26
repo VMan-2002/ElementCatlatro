1b9489efe76111835b2b20dc26cdd8a9045b4389
VMan-2002 <vman.2002.au@gmail.com>
2026-02-27 10:29:31 +1100

    me when i misinput

--- lua/levels/challenges.lua
+++ lua/levels/challenges.lua
@@ -7,7 +7,7 @@ SMODS.Challenge {
 			{id="discards", value = 2}
 		},
 		custom = {
-			{id="scaling", value = 600000},
+			{id="scaling", value = 1300000},
 			{id="enable_perishables_in_shop"}
 		}
 	},
