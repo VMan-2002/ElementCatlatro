a1a7f6f19de16411ec00d61b81d054510086d378
VMan-2002 <vman.2002.au@gmail.com>
2026-04-30 16:11:48 +1000

    support multiple lose quips

--- lua/blindside/bs_jokers.lua
+++ lua/blindside/bs_jokers.lua
@@ -97,7 +97,12 @@ SMODS.JimboQuip({
 	},
 	filter = function(self, type)
 		if G.GAME.modifiers.bs_ecattos_stake then
-			self.extra.text_key = string.sub(G.GAME.blind.name, 4) .. "_lose1"
+			local k_base, i = string.sub(G.GAME.blind.name, 4) .. "_lose", 1
+			while G.localization.misc.quips[k_base .. i] do
+				i = i + 1
+			end
+			if i == 1 then return false end
+			self.extra.text_key = i == 2 and (k_base .. "1") or (k_base .. math.random(i - 1))
 			self.extra.center = G.P_BLINDS[G.GAME.blind.name].ecattos_conf.my_center or "j_ecattos_element0"
 			return true, {override_base_checks = true, weight = 400}
 		end
